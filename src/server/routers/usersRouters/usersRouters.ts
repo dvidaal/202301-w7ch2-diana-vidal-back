b18e334aab2dc9f8418043fe0156b435413c5abb
Diana Vidal <dvidaal@gmail.com>
2023-02-23 20:16:16 +0100

    Install multer and create structure for avatar

--- src/server/routers/usersRouters/usersRouters.ts
+++ src/server/routers/usersRouters/usersRouters.ts
@@ -1,7 +1,23 @@
 import { Router } from "express";
-import { loginUser } from "../../controllers/usersControllers/usersControllers";
+import {
+  createUser,
+  loginUser,
+} from "../../controllers/usersControllers/usersControllers.js";
+import multer from "multer";
+
 const usersRouter = Router();
 
+const storage = multer.diskStorage({
+  destination(req, file, callback) {
+    callback(null, "/uploads");
+  },
+  filename(req, file, callback) {
+    callback(null, file.filename + "jpeg");
+  },
+});
+export const upload = multer({ storage });
+
 usersRouter.post("/login", loginUser);
+usersRouter.post("/create", upload.single("avatar"), createUser);
 
 export default usersRouter;
